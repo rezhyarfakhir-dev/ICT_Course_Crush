5c7f046854c7fd4beddad3db4f580cefba9edf11
rezhyarfakhir-dev <rezhyar.fakhir@gmail.com>
2026-04-14 09:09:35 +0000

    Add debugging information and loading state for GH Pages

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles/index.css'
 
+console.log('React Kurdish Map app starting...')
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
